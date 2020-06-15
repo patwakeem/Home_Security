c52a0381306b86aaca3201734124b9d652172a8e
Dimitris Zarris <dimzarris@hotmail.com>
2020-06-16 00:30:34 +0200

    Deleted wrongly added folder.

--- Home_Security/SecurityController/UnoMainController/Definitions.h
+++
@@ -1,9 +0,0 @@
-// This file contains definitions for the serial communication between the MEGA2560 
-// and the ESP32 embedded controller.
-
-const MSG_CONNECTION = "@C";
-const MSG_ALARM_STATUS = "@A";
-const MSG_PERSON_VERIFICATION = "@V";
-const MSG_KEYPAD_TYPING = "@K";
-const MSG_KEEP_ALIVE = "@L";
-const MSG_BATTERY_LEVEL = "@B";
