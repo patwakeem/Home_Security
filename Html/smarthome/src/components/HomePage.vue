c8fa9b306d3fff0db219050da237973aca060af4
Dimitris <dimzarris@hotmail.com>
2020-10-16 20:38:40 +0200

    Added more controllers and menu items.

--- Html/smarthome/src/components/HomePage.vue
+++ Html/smarthome/src/components/HomePage.vue
@@ -1,30 +1,80 @@
 <template>
-  <div class="home">
-    <div>
-      <img class="robot" src="../assets/robot-home.png"
-       aria-hidden="true">
-    </div>
-    <div class="get-started">
-      <a href="">Get started</a> building your first robot!
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col sm="12">
+        <v-card
+          elevation="4"
+          height="200">
+          <v-card-title>
+            Security Alarm
+          </v-card-title>
+          <v-card-actions>
+            <v-btn-toggle>
+              <!--v-img src="./assets/disarmed_shade.png" >
+
+              </v-img-->
+              Disarm
+            </v-btn-toggle>
+            <v-spacer></v-spacer>
+            <v-btn-toggle>
+              <!--v-img src="./assets/disarmed_shade.png" >
+
+              </!--v-img-->
+              Arm in
+            </v-btn-toggle>
+            <v-spacer></v-spacer>
+            <v-btn-toggle>
+              <!--v-img src="./assets/disarmed_shade.png" >
+
+              </!--v-img-->
+              Arm out
+            </v-btn-toggle>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="4">
+        <v-card
+          elevation="4"
+          height="200">
+          <v-card-title>
+            Temperature
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card
+          elevation="4"
+          height="200">
+          <v-card-title>
+            Humidity
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card
+          elevation="4"
+          height="200">
+          <v-card-title>
+            Air quality
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HomePage',
-  props: {
-    msg: String,
+  name: 'Dashboard',
+  created() {
+    this.$vuetify.theme.dark = true;
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.home {
-  text-align: center;
-}
-.robot {
-  height: 300px;
-}
+
 </style>
---
+++ Html/smarthome/src/components/Sensors.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-card
+          elevation="4"
+          height="200">
+          <v-card-title>
+            Sensors
+          </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="4">
+        <v-card
+          elevation="4"
+          height="200">
+          <v-card-title>
+            Cameras
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
